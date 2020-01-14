cc48020fb93344472a783938c4824037f2641436
Anton Sukhovatkin <artonio@pop-os.localdomain>
2020-01-14 11:15:29 -0500

    first commit

---
+++ src/installed_app_finder.rs
@@ -0,0 +1,188 @@
+use std::collections::HashMap;
+use glib::GString;
+use std::path::Path;
+use gio::prelude::*;
+use gtk::prelude::*;
+
+use gio::{AppInfo, DesktopAppInfo, Icon};
+use gtk::{IconTheme, IconLookupFlags};
+use gdk_pixbuf::Pixbuf;
+use regex::Regex;
+
+#[derive(Debug)]
+pub struct AppDetails {
+    pub app_name: String,
+    pub cmd: String,
+    pub icon: Icon,
+    pub icon_file_path: String,
+    pub icon_pix_buf: Option<Pixbuf>
+}
+
+
+impl PartialEq for AppDetails {
+    fn eq(&self, other: &Self) -> bool {
+        self.app_name == other.app_name && self.icon_file_path == other.icon_file_path
+    }
+}
+
+pub struct InstalledAppsFinder {
+    pub applications: Option<HashMap<String, Vec<AppDetails>>>
+}
+
+impl InstalledAppsFinder {
+    pub fn new() -> Self {
+        let mut iaf = InstalledAppsFinder {
+            applications: None
+        };
+        iaf.set_applications();
+        iaf
+    }
+
+    pub fn get_apps_for_category(&self, category: String) -> Vec<&AppDetails> {
+        let mut result = Vec::new();
+        if let Some(app) = &self.applications {
+            let cartegory_core_apps = app.get(&category.to_string()).unwrap();
+            for app in cartegory_core_apps {
+                result.push(app);
+            }
+        }
+        return result;
+    }
+
+    fn get_pixbuf_from_file_path(&self, file_path: &String) -> Option<Pixbuf> {
+        let path = Path::new(file_path);
+        // let pixbuf = Pixbuf::new_from_file(path).ok();
+        let pixbuf = Pixbuf::new_from_file_at_size(path, 64, 64).ok();
+        return pixbuf;
+    }
+
+    fn lookup_icon_using_theme(&self, icon_name: GString) -> Option<std::path::PathBuf> {
+        // Lookup icon using Gtk Theme
+        let icon_theme: Option<IconTheme> = IconTheme::get_default();
+        
+        if let Some(it) = icon_theme {
+            let icon_name_str = String::from(icon_name);
+            let icon_info = it.lookup_icon(icon_name_str.as_str(), 64, IconLookupFlags::FORCE_SVG);
+            let filename = icon_info.and_then(|icon_info| {
+                return icon_info.get_filename();
+            });
+            return filename;
+        }
+        return None;
+    }
+
+    fn get_icon_file_path(&self, icon: &Icon) -> String {
+        let mut new_file_name: String = String::from("");
+        let icon_str: Option<GString> = IconExt::to_string(icon);
+        if let Some(is) = icon_str {
+            let icon_string = is.as_str();
+            let icon_str_path = Path::new(icon_string);
+            if icon_str_path.exists() {
+               new_file_name = String::from(is);
+            } else {
+                let f_name = self.lookup_icon_using_theme(is);
+                if let Some(fname) = f_name {
+                    new_file_name = String::from(fname.to_str().unwrap());
+                }
+            }
+
+        }
+
+        return new_file_name;
+    }
+
+    pub fn pretty_print(&self) {
+        if let Some(a) = &self.applications {
+            for (key, _) in a.iter() {
+                println!("---- Category: {}", key);
+                for app_details in a.get(key).unwrap() {
+                    let ic = IconExt::to_string(&app_details.icon);
+                    let ic_str_opt = ic.unwrap();
+                    let ic_str = ic_str_opt.as_str();
+                    println!("Name: {}, {}, icon: {}, icon path: {}", app_details.app_name, app_details.cmd, ic_str, app_details.icon_file_path);
+                }
+            }
+        }
+    }
+
+    fn set_applications(&mut self) {
+        self.applications = Some(self.get_installed_apps());
+    }
+
+    fn get_installed_apps(&self)-> HashMap<String, Vec<AppDetails>> {
+        let mut applications: HashMap<String, Vec<AppDetails>> = HashMap::new();
+        let app_info_list: Vec<AppInfo> = AppInfo::get_all();
+        for app_info in app_info_list.iter() {
+            
+            let (categories, icon) = self.get_icon_and_categories(app_info);
+    
+            if icon.is_none() || categories.is_none() {
+                continue;
+            }
+    
+            if let (Some(cat), Some(icon)) = (categories, icon) {
+                let icon_file_path = self.get_icon_file_path(&icon);
+                let icon_pix_buf = self.get_pixbuf_from_file_path(&icon_file_path);
+    
+                let command_line = app_info.get_commandline();
+                // Returns command for the app, can be either ie. (gnome-contacts)
+                // or full path i.e /usr/share/code/code --no-sandbox --unity-launch
+                let cmd_string: Option<String> = command_line.and_then(|cl| {
+                    let cl_str_res = cl.into_os_string().into_string();
+                    return cl_str_res.ok();
+                });
+    
+                if let Some(cmd_str) = cmd_string {
+                    // Remove "%U" and "%F" placeholders
+                    let command = self.clean_command_for_file(&cmd_str);
+                    let name = app_info.get_name().unwrap();
+    
+                    let app_details = AppDetails {
+                        app_name: String::from(name),
+                        cmd: command.into_owned(),
+                        icon: icon,
+                        icon_file_path: icon_file_path,
+                        icon_pix_buf: icon_pix_buf
+                    };
+    
+                    let split_cat = cat.as_str().split(";");
+                    for category in split_cat {
+                        if category.trim() != "GTK" && category.trim() != "GNOME" && category.trim() != "" {
+                            if !applications.contains_key(&category.to_string()) {
+                                applications.insert(category.to_string(), Vec::new());
+                            }
+                            let app_cat = applications.get_mut(&category.to_string());
+                            if let Some(ac) = app_cat {
+                                // Notes: had to impl PartialEq for AppDetails
+                                if !ac.contains(&app_details) {
+                                    ac.push(app_details);
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+    
+        }
+        return applications;
+    }
+    
+
+    fn get_icon_and_categories(&self, app_info: &AppInfo) -> (Option<GString>, Option<Icon>) {
+        let icon = app_info.get_icon();
+        let app_id = app_info.get_id();
+        let categories = app_id.and_then(|ai| {
+            return DesktopAppInfo::new(&ai);
+        }).and_then(|dai| {
+            return dai.get_categories();
+        });
+    
+        return (categories, icon);
+    }
+
+    fn clean_command_for_file<'t>(&self, cl_str: &'t String) -> std::borrow::Cow<'t, str> {
+        let re = Regex::new("%\\w").unwrap();
+        return re.replace(cl_str.as_str(), "");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,106 @@
+extern crate gio;
+extern crate gtk;
+extern crate regex;
+
+
+use gio::prelude::*;
+use gtk::prelude::*;
+
+use std::env::args;
+use gio::{AppInfoExt};
+
+use glib::GString;
+
+mod installed_app_finder;
+use installed_app_finder::InstalledAppsFinder;
+
+enum IconViewColumnType {
+    TextColumn = 0,
+    PixbufColumn = 1,
+    CmdColumn = 2
+}
+
+
+fn main() {
+    let application = gtk::Application::new(
+        Some("com.github.gtk-rs.examples.iconview_example"),
+        Default::default(),
+    )
+    .expect("Initialization failed...");
+
+    application.connect_activate(|app| {
+        build_ui(app);
+    });
+
+    application.run(&args().collect::<Vec<_>>());
+}
+
+fn build_ui(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(application);
+
+    window.set_title("IconView Example");
+    window.set_border_width(10);
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(500, 500);
+
+    let icon_view = gtk::IconView::new();
+    icon_view.set_item_padding(0);
+    icon_view.set_columns(3);
+    icon_view.set_column_spacing(0);
+    // User can select only one item at a time
+    icon_view.set_selection_mode(gtk::SelectionMode::Single);
+
+    // 0 = App name, 1 = Pixbuf Icon, 2 = cmd
+    let col_types: [glib::Type; 3] = [glib::Type::String, gdk_pixbuf::Pixbuf::static_type(), glib::Type::String];
+    let icon_view_model = gtk::ListStore::new(&col_types);
+    icon_view.set_model(Some(&icon_view_model));
+
+    let iaf = InstalledAppsFinder::new();
+    iaf.pretty_print();
+
+    let core_apps = iaf.get_apps_for_category("Office".to_string());
+    for cp in core_apps {
+        icon_view_model.insert_with_values(
+            None,
+            &[
+                IconViewColumnType::TextColumn as u32,
+                IconViewColumnType::PixbufColumn as u32,
+                IconViewColumnType::CmdColumn as u32
+            ],
+            &[&cp.app_name, &cp.icon_pix_buf, &cp.cmd]
+        );
+    }
+    icon_view.set_activate_on_single_click(true);
+    icon_view.connect_item_activated(|widget, path| {
+        let icon_view_model_res = widget.get_model().unwrap().downcast::<gtk::ListStore>();
+        let icon_view_model = icon_view_model_res.ok().unwrap();
+
+        let iter = icon_view_model.get_iter(path).unwrap();
+        let iter_str = icon_view_model.get_string_from_iter(&iter).unwrap();
+        let val = icon_view_model.get_value(&iter, 2);
+        let t: Result<glib::value::TypedValue<String>, glib::value::Value>  = val.downcast();
+        match t {
+            Ok(o) => {
+                let command = o.get().unwrap();
+                let a_info = gio::AppInfo::create_from_commandline(command.as_str(), None, gio::AppInfoCreateFlags::NONE).ok();
+                if let Some(a) = a_info {
+                    let empty_file_array: [gio::File; 0] = [];
+                    let result: Result<(), glib::error::Error> = a.launch(&empty_file_array, Some(&gio::AppLaunchContext::new()));
+                    match result {
+                        Ok(o) => {},
+                        Err(e) => println!("{:?}", e)
+                    }
+                }
+                // a.launch(None, None);
+                // println!("{:?}", o.get().unwrap());
+            },
+            Err(e) => {println!("{:?}", e)}
+        }
+    });
+
+    icon_view.set_text_column(IconViewColumnType::TextColumn as i32);
+    icon_view.set_pixbuf_column(IconViewColumnType::PixbufColumn as i32);
+
+    window.add(&icon_view);
+    window.show_all();
+}
