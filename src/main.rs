5dba17d1b83e1666822c103127caebd00b3a7c80
artonio <artonio@pop-os.localdomain>
2020-01-15 13:46:31 -0500

    refactor to use cascade crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate gio;
 extern crate gtk;
 extern crate regex;
-
+extern crate cascade;
+use cascade::cascade;
 
 use gio::prelude::*;
 use gtk::prelude::*;
@@ -36,24 +37,65 @@ fn main() {
 }
 
 fn build_ui(application: &gtk::Application) {
-    let window = gtk::ApplicationWindow::new(application);
 
-    window.set_title("IconView Example");
-    window.set_border_width(10);
-    window.set_position(gtk::WindowPosition::Center);
-    window.set_default_size(500, 500);
+    let search_btn = gtk::Button::new_from_icon_name(Some("spacefm-find"), 
+                                                    gtk::IconSize::SmallToolbar);
 
-    let icon_view = gtk::IconView::new();
-    icon_view.set_item_padding(0);
-    icon_view.set_columns(3);
-    icon_view.set_column_spacing(0);
-    // User can select only one item at a time
-    icon_view.set_selection_mode(gtk::SelectionMode::Single);
+    let header_bar = cascade! {
+        gtk::HeaderBar::new();
+        ..set_show_close_button(true);
+        ..set_title(Some("IconView Example"));
+    };
+    header_bar.pack_end(&search_btn);
 
+    let window = cascade! {
+        gtk::ApplicationWindow::new(application);
+        ..set_titlebar(Some(&header_bar));
+        ..set_border_width(10);
+        ..set_position(gtk::WindowPosition::Center);
+        ..set_default_size(500, 500);
+    };
+    
     // 0 = App name, 1 = Pixbuf Icon, 2 = cmd
     let col_types: [glib::Type; 3] = [glib::Type::String, gdk_pixbuf::Pixbuf::static_type(), glib::Type::String];
     let icon_view_model = gtk::ListStore::new(&col_types);
-    icon_view.set_model(Some(&icon_view_model));
+
+    let icon_view = cascade! {
+        gtk::IconView::new();
+        ..set_item_padding(0);
+        ..set_columns(3);
+        ..set_column_spacing(0);
+        // User can select only one item at a time
+        ..set_selection_mode(gtk::SelectionMode::Single);
+        ..set_activate_on_single_click(true);
+        ..set_model(Some(&icon_view_model));
+        ..connect_item_activated(|widget, path| {
+            let icon_view_model_res = widget.get_model().unwrap().downcast::<gtk::ListStore>();
+            let icon_view_model = icon_view_model_res.ok().unwrap();
+    
+            let iter = icon_view_model.get_iter(path).unwrap();
+            let val = icon_view_model.get_value(&iter, IconViewColumnType::CmdColumn as i32);
+            let t: Result<glib::value::TypedValue<String>, glib::value::Value>  = val.downcast();
+            match t {
+                Ok(o) => {
+                    let command = o.get().unwrap();
+                    let a_info = gio::AppInfo::create_from_commandline(command.as_str(), None, gio::AppInfoCreateFlags::NONE).ok();
+                    if let Some(a) = a_info {
+                        let empty_file_array: [gio::File; 0] = [];
+                        let result: Result<(), glib::error::Error> = a.launch(&empty_file_array, Some(&gio::AppLaunchContext::new()));
+                        match result {
+                            Ok(o) => {},
+                            Err(e) => println!("{:?}", e)
+                        }
+                    }
+                },
+                Err(e) => {println!("{:?}", e)}
+            }
+        });
+        ..set_text_column(IconViewColumnType::TextColumn as i32);
+        ..set_pixbuf_column(IconViewColumnType::PixbufColumn as i32);
+    };
+
 
     let iaf = InstalledAppsFinder::new();
     iaf.pretty_print();
@@ -70,36 +112,6 @@ fn build_ui(application: &gtk::Application) {
             &[&cp.app_name, &cp.icon_pix_buf, &cp.cmd]
         );
     }
-    icon_view.set_activate_on_single_click(true);
-    icon_view.connect_item_activated(|widget, path| {
-        let icon_view_model_res = widget.get_model().unwrap().downcast::<gtk::ListStore>();
-        let icon_view_model = icon_view_model_res.ok().unwrap();
-
-        let iter = icon_view_model.get_iter(path).unwrap();
-        let iter_str = icon_view_model.get_string_from_iter(&iter).unwrap();
-        let val = icon_view_model.get_value(&iter, 2);
-        let t: Result<glib::value::TypedValue<String>, glib::value::Value>  = val.downcast();
-        match t {
-            Ok(o) => {
-                let command = o.get().unwrap();
-                let a_info = gio::AppInfo::create_from_commandline(command.as_str(), None, gio::AppInfoCreateFlags::NONE).ok();
-                if let Some(a) = a_info {
-                    let empty_file_array: [gio::File; 0] = [];
-                    let result: Result<(), glib::error::Error> = a.launch(&empty_file_array, Some(&gio::AppLaunchContext::new()));
-                    match result {
-                        Ok(o) => {},
-                        Err(e) => println!("{:?}", e)
-                    }
-                }
-                // a.launch(None, None);
-                // println!("{:?}", o.get().unwrap());
-            },
-            Err(e) => {println!("{:?}", e)}
-        }
-    });
-
-    icon_view.set_text_column(IconViewColumnType::TextColumn as i32);
-    icon_view.set_pixbuf_column(IconViewColumnType::PixbufColumn as i32);
 
     window.add(&icon_view);
     window.show_all();
